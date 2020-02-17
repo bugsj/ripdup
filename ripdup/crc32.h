7fd32dc3be4e5e0449496eda01ef9d3dd2913eb8
罗捷 <luojie_zju@hotmail.com>
2020-02-17 19:53:34 +0800

    license

--- ripdup/crc32.h
+++ ripdup/crc32.h
@@ -1,3 +1,27 @@
+/*
+	ripdup - find and remove duplicate files in iso image file
+
+	Copyright(c) 2020 Luo Jie
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this softwareand associated documentation files(the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions :
+
+	The above copyright noticeand this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+ */
+
 #pragma once
 
 #include <intrin.h>
@@ -5,21 +29,24 @@
 inline unsigned int crc32(const void* buf, size_t size)
 {
 	const unsigned char* p = reinterpret_cast<const unsigned char*>(buf);
-	unsigned long long crc = 0;
+	unsigned long long crc64 = 0;
+	unsigned int crc = 0;
 
 	while (size) {
 		if ((reinterpret_cast<unsigned long long>(p) & 0x7) == 0 && size >= 8) {
+			crc64 = crc;
 			while (size >= 8) {
-				crc = _mm_crc32_u64(crc, *reinterpret_cast<const unsigned long long*>(p));
+				crc64 = _mm_crc32_u64(crc64, *reinterpret_cast<const unsigned long long*>(p));
 				p += 8;
 				size -= 8;
 			}
+			crc = static_cast<unsigned int>(crc64);
 		}
 		else {
-			crc = _mm_crc32_u8(static_cast<unsigned int>(crc), *p++);
+			crc = _mm_crc32_u8(crc, *p++);
 			--size;
 		}
 	}
 
-	return static_cast<unsigned int>(crc);
+	return crc;
 }
